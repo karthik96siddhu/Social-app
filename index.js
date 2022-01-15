3ab0977e7e6d30f871ca09b8fa8e680e392df537
karthik96siddhu <karthik96siddhu@gmail.com>
2022-01-15 19:03:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const dateFormat = require('date-format')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>Hello World</h1>')
+})
+
+app.get('/api/v1/instagram', (req, res) => {
+    const instaSocial = {
+        username: 'karthik_siddhu',
+        followers: 120,
+        follows: 280,
+        date: dateFormat.asString('dd-MM-yyyy hh:mm:ss.SSS', new Date())
+    }
+    res.status(200).json(instaSocial)
+})
+
+app.get('/api/v1/facebook', (req, res) => {
+    const instaSocial = {
+        username: 'karthik_siddhu',
+        followers: 70,
+        follows: 130,
+        date: dateFormat.asString('dd-MM-yyyy hh:mm:ss.SSS', new Date())
+    }
+    res.status(200).json(instaSocial)
+})
+app.get('/api/v1/linkedin', (req, res) => {
+    const instaSocial = {
+        username: 'karthik_shivaram',
+        followers: 600,
+        follows: 670,
+        date: dateFormat.asString('dd-MM-yyyy hh:mm:ss.SSS', new Date())
+    }
+    res.status(200).json(instaSocial)
+})
+
+app.get('/api/v1/:token', (req, res) => {
+    res.status(200).json({param: req.params.token})
+})
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
+})
